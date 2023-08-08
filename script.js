438b8a38af1d1f51912306a8c99837586b4d3ef3
Sidney Souza <84251707+sidneysouzadg@users.noreply.github.com>
2023-08-08 19:24:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+  if(sections.length) {
+    const windowMetade = window.innerHeight * 0.8;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - windowMetade) < 0;
+        if(isSectionVisible)
+          section.classList.add('ativo');
+        else 
+          section.classList.remove('ativo');
+      })
+    }
+
+    animaScroll();
+
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+initAnimacaoScroll();
+
+
+function toggleAccordion(header) {
+  var content = header.nextElementSibling;
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
+}
